a2136349944654335fec261d619ad146df39edee
TapAlexxx <lordpictures123@gmail.com>
2023-04-07 22:56:35 +0300

    basic cellView

---
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/GridControl/StateMachine/States/CellView.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BuilderGame.Gameplay.GridControl.StateMachine.States
+{
+    public class CellView : MonoBehaviour
+    {
+        [SerializeField] private GameObject viewObject;
+    }
+}
