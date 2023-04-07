2ca2f56d8e388543a2abda0b071516df3859c64b
TapAlexxx <lordpictures123@gmail.com>
2023-04-07 21:10:09 +0300

    implement basic cell state machine

---
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/GridControl/StateMachine/States/CellState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace BuilderGame.Gameplay.GridControl.StateMachine.States
+{
+    public abstract class CellState : MonoBehaviour
+    {
+        public abstract event Action ReadyToNextState;
+        public abstract void Enter();
+        public abstract void Exit();
+    }
+}
