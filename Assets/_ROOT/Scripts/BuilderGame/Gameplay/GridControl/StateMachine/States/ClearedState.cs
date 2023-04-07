7982e16faf8dc7889ea4f009c114897524f31a72
TapAlexxx <lordpictures123@gmail.com>
2023-04-07 22:56:11 +0300

    created two states for tests(tests succeeded)

---
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/GridControl/StateMachine/States/ClearedState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace BuilderGame.Gameplay.GridControl.StateMachine.States
+{
+    public class ClearedState : CellState
+    {
+        public override event Action ReadyToNextState;
+
+        public override void Enter()
+        {
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+}
---
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/GridControl/StateMachine/States/GrassState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace BuilderGame.Gameplay.GridControl.StateMachine.States
+{
+    public class GrassState : CellState
+    {
+        public override event Action ReadyToNextState;
+
+        public override void Enter()
+        {
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+}
