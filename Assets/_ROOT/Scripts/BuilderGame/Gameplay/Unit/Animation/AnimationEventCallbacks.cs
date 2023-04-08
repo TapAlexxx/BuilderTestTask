281ea56cdfc86d0433f0975d8bad4895f9e8150d
TapAlexxx <lordpictures123@gmail.com>
2023-04-08 21:02:44 +0300

    implemented animation callbacks

---
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/Unit/Animation/AnimationEventCallbacks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace BuilderGame.Gameplay.Unit.Animation
+{
+    public class AnimationEventCallbacks : MonoBehaviour
+    {
+        public event Action Plowed;
+        public event Action Planted;
+
+        [UsedImplicitly]
+        public void PlowEnded()
+        {
+            Plowed?.Invoke();
+        }
+        
+        [UsedImplicitly]
+        public void PlantEnded()
+        {
+            Planted?.Invoke();
+        }
+    }
+}
