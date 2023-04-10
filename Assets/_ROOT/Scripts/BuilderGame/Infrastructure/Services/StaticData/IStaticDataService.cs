227e69951e906fd24273cf94518b26e578b0b54f
TapAlexxx <lordpictures123@gmail.com>
2023-04-10 20:58:26 +0300

    add GetplantData method in static data service

--- Assets/_ROOT/Scripts/BuilderGame/Infrastructure/Services/StaticData/IStaticDataService.cs
+++ Assets/_ROOT/Scripts/BuilderGame/Infrastructure/Services/StaticData/IStaticDataService.cs
@@ -1,7 +1,11 @@
-﻿namespace BuilderGame.Infrastructure.Services.StaticData
+﻿using BuilderGame.StaticData;
+using BuilderGame.StaticData.Plants;
+
+namespace BuilderGame.Infrastructure.Services.StaticData
 {
     public interface IStaticDataService
     {
         void LoadData();
+        PlantStaticData GetPlantStaticData(PlantType plantType);
     }
 }
