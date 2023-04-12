22f8ff68ad0219036e0d088502b27dbaeb2132c0
TapAlexxx <lordpictures123@gmail.com>
2023-04-12 11:30:00 +0300

    decomposed particle animations

---
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/Unit/Animation/HarvestParticleAnimation.cs
@@ -0,0 +1,26 @@
+﻿using BuilderGame.Gameplay.Unit.CellInteraction;
+using UnityEngine;
+
+namespace BuilderGame.Gameplay.Unit.Animation
+{
+    public class HarvestParticleAnimation : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem harvestParticle;
+        [SerializeField] private UnitHarvester unitHarvester;
+
+        private void OnValidate()
+        {
+            unitHarvester = GetComponentInChildren<UnitHarvester>();
+        }
+
+        private void Start() =>
+            unitHarvester.Harvested += AnimateHarvest;
+
+        private void OnDestroy() => 
+            unitHarvester.Harvested -= AnimateHarvest;
+
+
+        private void AnimateHarvest() => 
+            harvestParticle.Play();
+    }
+}
---
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/Unit/Animation/InteractableParticleAnimation.cs
@@ -0,0 +1,35 @@
+﻿using BuilderGame.Gameplay.Unit.CellInteraction;
+using UnityEngine;
+
+namespace BuilderGame.Gameplay.Unit.Animation
+{
+    public class InteractableParticleAnimation : MonoBehaviour
+    {
+        [SerializeField] private CellInteractable cellInteractor;
+        [SerializeField] private ParticleSystem interactionParticle;
+
+        private void Start()
+        {
+            cellInteractor.StartedInteract += StartAnimate;
+            cellInteractor.EndedInteract += StopAnimate;
+        }
+
+        private void OnDestroy()
+        {
+            cellInteractor.StartedInteract -= StartAnimate;
+            cellInteractor.EndedInteract -= StopAnimate;
+        }
+
+        private void StartAnimate() => 
+            SetEmissionState(interactionParticle, true);
+
+        private void StopAnimate() => 
+            SetEmissionState(interactionParticle, false);
+
+        private void SetEmissionState(ParticleSystem particle, bool state)
+        {
+            var emission = particle.emission;
+            emission.enabled = state;
+        }
+    }
+}
